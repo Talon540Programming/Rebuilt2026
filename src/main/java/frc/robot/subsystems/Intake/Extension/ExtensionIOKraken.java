117ee1dc87ae89a6f243296d3b97da781adea1e1
DrollerBubble59 <114955979+DrollerBubble59@users.noreply.github.com>
2026-01-21 10:01:07 -0500

    removing unneeded imports

--- src/main/java/frc/robot/subsystems/Intake/Extension/ExtensionIOKraken.java
+++ src/main/java/frc/robot/subsystems/Intake/Extension/ExtensionIOKraken.java
@@ -3,7 +3,6 @@ package frc.robot.subsystems.Intake.Extension;
 import com.ctre.phoenix6.BaseStatusSignal;
 import com.ctre.phoenix6.StatusSignal;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
-import com.ctre.phoenix6.controls.DutyCycleOut;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
